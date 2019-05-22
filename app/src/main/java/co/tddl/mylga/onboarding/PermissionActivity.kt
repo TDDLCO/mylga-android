7ba881c4d5eb4898c9ba340ea9d831623f1d8cd2
nennenwodo <nennenwodo@gmail.com>
2019-05-22 09:22:16 +0100

    Added loading screen to permission activity

--- app/src/main/java/co/tddl/mylga/onboarding/PermissionActivity.kt
+++ app/src/main/java/co/tddl/mylga/onboarding/PermissionActivity.kt
@@ -2,8 +2,8 @@ package co.tddl.mylga.onboarding
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import com.google.android.material.snackbar.Snackbar
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity;
 import co.tddl.mylga.HomeActivity
 import co.tddl.mylga.R
@@ -22,6 +22,9 @@ class PermissionActivity : AppCompatActivity() {
         text_view_greeting.text = "Hi $username, nice to meet you."
 
         btn_next.setOnClickListener {
+            permission_loading_screen.visibility = View.VISIBLE
+            window.setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE, WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
             finish()
