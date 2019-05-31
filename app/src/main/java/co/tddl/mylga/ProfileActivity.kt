757055a28b5e5f2969dcd4576fcab7a67cb1e43a
nennenwodo <nennenwodo@gmail.com>
2019-05-31 22:10:48 +0100

    Added user details firestore update

--- app/src/main/java/co/tddl/mylga/ProfileActivity.kt
+++ app/src/main/java/co/tddl/mylga/ProfileActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
-
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -17,6 +16,8 @@ import kotlinx.android.synthetic.main.content_profile.*
 
 class ProfileActivity : AppCompatActivity() {
 
+    private var docId: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
@@ -25,32 +26,10 @@ class ProfileActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         fetchUserDetails()
 
-        val shortAnimationDuration = resources.getInteger(android.R.integer.config_shortAnimTime)
-
         image_view_profile.setOnClickListener {
-
             appbar.visibility = View.GONE
             nested_scroll_view_content.visibility = View.GONE
-
-            relative_layout_expanded_image.apply {
-                alpha = 0.85f
-                visibility = View.VISIBLE
-
-                animate()
-                    .alpha(1f)
-                    .setDuration(shortAnimationDuration.toLong())
-                    .setListener(null)
-            }
-
-            nested_scroll_view_content.animate()
-                .alpha(1f)
-                .setDuration(shortAnimationDuration.toLong())
-                .setListener(object : AnimatorListenerAdapter() {
-                    override fun onAnimationEnd(animation: Animator) {
-                        appbar.visibility = View.GONE
-                        nested_scroll_view_content.visibility = View.GONE
-                    }
-                })
+            animateZoom()
         }
 
         image_button_close.setOnClickListener {
@@ -59,7 +38,31 @@ class ProfileActivity : AppCompatActivity() {
             relative_layout_expanded_image.visibility = View.GONE
         }
 
-        btn_update_profile.setOnClickListener {  }
+        btn_update_profile.setOnClickListener { updateUserDetails() }
+    }
+
+    private fun animateZoom(){
+        val shortAnimationDuration = resources.getInteger(android.R.integer.config_shortAnimTime)
+
+        relative_layout_expanded_image.apply {
+            alpha = 0.85f
+            visibility = View.VISIBLE
+
+            animate()
+                .alpha(1f)
+                .setDuration(shortAnimationDuration.toLong())
+                .setListener(null)
+        }
+
+        nested_scroll_view_content.animate()
+            .alpha(1f)
+            .setDuration(shortAnimationDuration.toLong())
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    appbar.visibility = View.GONE
+                    nested_scroll_view_content.visibility = View.GONE
+                }
+            })
     }
 
     private fun fetchUserDetails(){
@@ -69,9 +72,9 @@ class ProfileActivity : AppCompatActivity() {
             .whereEqualTo("id", auth.currentUser?.uid.toString())
             .get()
             .addOnSuccessListener { documents ->
+                docId = documents.first().id
                 val username = documents.first()["name"].toString()
-                edit_text_user_name.setText(username)
-                htab_collapse_toolbar.title = username
+                showUserDetails(username)
             }
             .addOnFailureListener { exception ->
                 Log.w("Error", "Error getting documents: ", exception)
@@ -79,12 +82,32 @@ class ProfileActivity : AppCompatActivity() {
             }
     }
 
-    private fun showUserDetails(){
-
+    private fun showUserDetails(username: String){
+        edit_text_user_name.setText(username)
+        htab_collapse_toolbar.title = username
     }
 
     private fun updateUserDetails(){
+        if(docId.isNullOrEmpty()){
+            Toast.makeText(this, "An error occurred. Could not update details", Toast.LENGTH_LONG).show()
+            return
+        }
+        val auth = FirebaseAuth.getInstance()
+        val db = FirebaseFirestore.getInstance()
 
+        val user = HashMap<String, Any>()
+        user["id"] = auth.currentUser?.uid.toString()
+        user["name"] = edit_text_user_name.text.toString()
+
+        db.collection("users").document(docId!!)
+            .set(user)
+            .addOnSuccessListener {
+                htab_collapse_toolbar.title = user["name"].toString()
+                Toast.makeText(applicationContext, "Done", Toast.LENGTH_LONG).show()
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(applicationContext, "Error. Could not save username", Toast.LENGTH_LONG).show()
+            }
     }
 
 }
