7c46b7bd751dcb3ef87062bf4239489e12287a91
nennenwodo <nennenwodo@gmail.com>
2019-05-31 19:12:30 +0100

    Added firebase logout

--- app/src/main/java/co/tddl/mylga/SettingsActivity.kt
+++ app/src/main/java/co/tddl/mylga/SettingsActivity.kt
@@ -3,11 +3,16 @@ package co.tddl.mylga
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import co.tddl.mylga.onboarding.RegistrationActivity
+import co.tddl.mylga.util.SharedPreferenceHelper
+import com.google.firebase.auth.FirebaseAuth
 
 import kotlinx.android.synthetic.main.activity_settings.*
 import kotlinx.android.synthetic.main.content_settings.*
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var sharedPrefHelper: SharedPreferenceHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +20,16 @@ class SettingsActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        
+
+        sharedPrefHelper = SharedPreferenceHelper(applicationContext)
+
+        btn_logout.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this, RegistrationActivity::class.java)
+            startActivity(intent)
+            finishAffinity()
+        }
+
     }
 
 }
