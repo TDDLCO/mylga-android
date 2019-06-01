6b3a45aa8660734f64d5f8fdae0f65f8f7f4f3dd
nennenwodo <nennenwodo@gmail.com>
2019-06-01 08:01:20 +0100

    Checkes/unchecks switch according to user settings

--- app/src/main/java/co/tddl/mylga/SettingsActivity.kt
+++ app/src/main/java/co/tddl/mylga/SettingsActivity.kt
@@ -2,10 +2,13 @@ package co.tddl.mylga
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import co.tddl.mylga.onboarding.RegistrationActivity
 import co.tddl.mylga.util.SharedPreferenceHelper
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 import kotlinx.android.synthetic.main.activity_settings.*
 import kotlinx.android.synthetic.main.content_settings.*
@@ -13,6 +16,7 @@ import kotlinx.android.synthetic.main.content_settings.*
 class SettingsActivity : AppCompatActivity() {
 
     private lateinit var sharedPrefHelper: SharedPreferenceHelper
+    private var docId: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +24,8 @@ class SettingsActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        fetchUserDetails()
+        addCheckedChangeListeners()
 
         sharedPrefHelper = SharedPreferenceHelper(applicationContext)
 
@@ -30,6 +36,39 @@ class SettingsActivity : AppCompatActivity() {
             finishAffinity()
         }
 
+    }
+
+    private fun addCheckedChangeListeners(){
+        switch_show_picture.setOnCheckedChangeListener { buttonView, isChecked ->  }
+        switch_show_location.setOnCheckedChangeListener { buttonView, isChecked ->  }
+    }
+
+    private fun fetchUserDetails(){
+        val auth = FirebaseAuth.getInstance()
+        val db = FirebaseFirestore.getInstance()
+        db.collection("users")
+            .whereEqualTo("id", auth.currentUser?.uid.toString())
+            .get()
+            .addOnSuccessListener { documents ->
+                docId = documents.first().id
+                val showLocation = documents.first()["showLocation"] as Boolean
+                val showPicture = documents.first()["showPicture"] as Boolean
+                showUiFeedback(showPicture, showLocation)
+            }
+            .addOnFailureListener { exception ->
+                Log.w("Error", "Error getting documents: ", exception)
+                Toast.makeText(this, "An error occurred. Could not fetch details", Toast.LENGTH_LONG).show()
+            }
+    }
+
+    private fun showUiFeedback(showPicture: Boolean, showLocation: Boolean){
+        if(showPicture){
+            switch_show_picture.isChecked = true
+        }
+
+        if(showLocation){
+            switch_show_location.isChecked = true
+        }
     }
 
 }
